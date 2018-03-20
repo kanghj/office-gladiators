28dad495551d080151bec1a7567c04112cdf9467
Kang Hong Jin <kanghongjin@gmail.com>
2018-03-20 23:28:08 +0800

    fix missing # in grammar

--- grammar/colleague_speak.js
+++ grammar/colleague_speak.js
@@ -3,7 +3,7 @@ var colleagueGrammar = tracery.createGrammar({
 	"officeItem": ["printer", "stapler", "chair", "coffee mug"],
 	"mood": ["vexed","indignant","impassioned","depressed"],
 	"greeting": ["Hey John,"],
-	"goAway": ["[slaveFavouriteItem:#officeItem#]greeting#.\nI am really #mood# right now, so can you go away?"]
+	"goAway": ["#greeting#.\nI am really #mood# right now, so can you go away?"]
 });
 
 function colleagueSpeaks() {
